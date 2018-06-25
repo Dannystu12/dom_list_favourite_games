353faf62b69aff9de291bfbead926a2927f5c32a
Daniel Stewart <mgdog1@googlemail.com>
2018-06-25 08:19:38 +0100

    complete

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
+
+
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('#new-item');
 
   const handleFormSubmit = function(event) {
     event.preventDefault();
     const form = this;
 
-    const title = document.createTextNode(this.querySelector('#title').value);
-    const developer = document.createTextNode(this.querySelector('#developer').value);
-    const platform = document.createTextNode(this.querySelector('#platform').value);
+    const title = this.querySelector('#title').value;
+    const developer = this.querySelector('#developer').value;
+    const platform = this.querySelector('#platform').value;
+
 
     const titleCell = document.createElement('td');
     const developerCell = document.createElement('td');
     const platformCell = document.createElement('td');
-    titleCell.appendChild(title);
-    developerCell.appendChild(developer);
-    platformCell.appendChild(platform);
+    titleCell.appendChild(document.createTextNode(title));
+    developerCell.appendChild(document.createTextNode(developer));
+    platformCell.appendChild(document.createTextNode(platform));
 
     const row = document.createElement('tr');
     row.appendChild(titleCell);
@@ -26,6 +28,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.reset();
   };
-
+  const form = document.querySelector('#new-item');
   form.addEventListener('submit', handleFormSubmit);
+
+  const handleDeleteAll = function(event) {
+    const table = document.querySelector('#game-list');
+    while(table.firstChild){
+      table.removeChild(table.firstChild);
+    }
+  };
+
+  const deleteAll = document.querySelector('#delete-all');
+  deleteAll.addEventListener('click', handleDeleteAll);
+
 });
